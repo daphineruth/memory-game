49ac00a79c3415fabbe473105fe464400c697324
sheiswiz <daphneruth@304gmail.com>
2022-03-15 20:02:05 +0300

    dded js card options

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+//card options
     const cardArray = {
-        
+        name: 'fries',
+        img:'images/fries.png'
     }
+    {
+        name: 'fries',
+        img:'images/fries.png'   
+    }
+    {
+        name: 'fries',
+        img :'images/fries.png'
+    }
+    {
+        name: 'fries',
+        img :'images/fries.png'
+    }
+
+    {
+        name: 'fries',
+        img :'images/fries.png'
+    }
+
 })
