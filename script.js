e22de1a2894ec8f5b177facc203e23dbce49c60e
sheiswiz <daphneruth@304gmail.com>
2022-03-15 20:50:00 +0300

    fixed script bugs

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 //card options
-    const cardArray = {
+    const cardArray = [
+     {
         name: 'fries',
         img:'images/fries.png'
-    }
-    {
-        name: 'fries',
-        img:'images/fries.png'   
-    }
-    {
-        name: 'fries',
-        img :'images/fries.png'
-    }
-    {
-        name: 'fries',
-        img :'images/fries.png'
-    }
+    },
 
     {
         name: 'fries',
-        img :'images/fries.png'
+        img:'images/fries.png'   
+    },
+
+    {
+        name: 'burger',
+        img :'images/burger.png'
+    },
+
+    {
+        name: 'hotdog',
+        img :'images/hotdog.png'
+    },
+
+    {
+        name: 'milkshake',
+        img :'images/milkshake.png'
     }
+]
 
 })
